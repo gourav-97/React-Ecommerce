2389a5fdb06115a2232398f2c62b448e2a016df0
Samyak Jain <jain.samyak7@gmail.com>
2019-01-27 14:20:08 +0530

    MS2-Cart

---
+++ src/Cart.js
@@ -0,0 +1,234 @@
+import React, { Component } from "react";
+import { Button, ButtonGroup, Container, Table } from "reactstrap";
+// import AppNavbar from "./AppNavbar";
+import axios from 'axios';
+
+class Cart extends Component {
+  emptyCart = {
+    item: [],
+    amountPayable: ""
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      // item: this.emptyItem,
+      count:"",
+      newCart: this.emptyCart
+    };
+    this.state = { newCart: [], isLoading: true };
+    this.remove = this.remove.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    this.setState({count:[]})
+    fetch("/cart")
+      .then(response => response.json())
+      .then(data => this.setState({ newCart: data, count : 1, isLoading: false }));
+  }
+
+  async remove(id,price) {
+    console.log("In delete method " + id);
+    // const{newCart,cartItemList = [newCart]} = this.state;
+    await fetch(`/cart/removeCartItem/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(() => {
+      console.log("anuj");
+      // console.log(this.state.cartItemList[0].item.productId);
+      let updatedCart = [...this.state.newCart.cartItemList].filter(
+        cartItemList => cartItemList.item.productId !== id
+      );
+      let updated = this.state.newCart
+      updated.cartItemList = updatedCart;
+      updated.amountPayable = updated.amountPayable - price;
+      console.log("PRice = "+price);
+      console.log("NEW CART + " + JSON.stringify(updated))
+      console.log("Updated cart is = " + JSON.stringify(updatedCart));
+      this.setState({ newCart : updated});
+    });
+  }
+
+  async placeOrder(cartList,newCart){
+    console.log("AA raha hai " + this.state.newCart);
+
+    axios.post('http://localhost:8080/placeOrder',
+           this.state.newCart,
+       {'Content-Type':'application/json'}).then(res=>{
+            console.log("REsponse = "+res)
+            console.log(res.data.statusCode)
+            if(res.data.statusCode===200){
+                console.log(res.data.responseData)
+                this.props.history.push({
+                  pathname:"/order",
+                  state:{
+                    responseData: res.data.responseData
+                  }
+                })
+            }
+            else{
+                window.location.reload();
+                alert(res.data.message);
+            }
+         }).catch(error=>{
+            console.log(error.response)
+            if(error.response)
+                console.log(error.response);
+        })
+  }
+
+
+  async removeAll() {
+    // console.log("In delete method " + id);
+    await fetch(`/cart/emptyCart`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(() => {
+      let updatedCart = [];
+      // this.setState({
+      //   newCart:updatedCart
+      // })
+    });
+    window.location.reload();
+  }
+
+  increment(item,newCart) {
+    console.log("INCREMENT BEFORE = "+item.quantity)
+    this.setState({
+      count: item.quantity + 1
+    });
+    item.quantity = item.quantity + 1
+    newCart.amountPayable = newCart.amountPayable + item.price
+    this.setState({
+      newCart:newCart
+    })
+    console.log("INCREMENT = "+this.state.count)
+    
+  };
+  
+  decrement(item,newCart) {
+    if(item.quantity===0)
+    {
+      return;
+    }
+    this.setState({
+      count: item.quantity - 1
+    });
+    item.quantity = item.quantity - 1
+    newCart.amountPayable = newCart.amountPayable - item.price
+    this.setState({
+      newCart:newCart
+    })
+  };
+
+
+  render() {
+    console.log("In render");
+    const { newCart, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+    console.log("In render before cartList + ");
+    const cartList = newCart.cartItemList.map(cartItem => {
+      return (
+        <tr key={cartItem.item.id}>
+          {console.log("Name = " + cartItem.item.price)}
+          <td style={{ whiteSpace: "nowrap" }}>{cartItem.item.productName}</td>
+          {/* <td>{cartItem.item.productName}</td> */}
+          <td>{cartItem.item.price}</td>
+          <td>
+          <div>
+       <button onClick={(e) => this.increment(cartItem.item,newCart)}>+</button>
+       {cartItem.item.quantity}
+        <button onClick={(e) => this.decrement(cartItem.item,newCart)}>-</button>
+      </div>
+            
+
+          </td>
+          <td>{cartItem.item.price * cartItem.item.quantity}</td>
+          {/* <td>{cartItem.item.description}</td> */}
+          {console.log("Cart  = " + newCart.amountPayable)}
+          {/* <td>{newCart.amountPayable}</td> */}
+          <td>
+            {/* {console.log(cart.item.id)} */}
+            <ButtonGroup>
+              <Button
+                size="sm"
+                color="danger"
+                onClick={() => this.remove(cartItem.item.productId,cartItem.item.price*cartItem.item.quantity)}
+              >
+                Delete
+              </Button>
+
+              {/* <Button size="sm" color="primary" tag={Link} to={"/groups/" + group.id}>Edit</Button> */}
+              {/* <Button size=/"sm" color="danger" onClick={() => this.remove(cart.item.id)}>Delete</Button> */}
+              {/* <Button
+                size="sm"
+                color="success"
+                type="submit"
+                onClick={() => this.removeAll()}
+              >
+                empty cart
+              </Button> */}
+            </ButtonGroup>
+          </td>
+          {/* <td>{newCart.amountPayable}</td> */}
+        </tr>
+      );
+    });
+
+    console.log("In render before return");
+    return (
+      <div>
+        {/* <AppNavbar /> */}
+        <Container fluid>
+          {console.log("In table")}
+          <h2>
+            <center>My Cart</center>
+          </h2>
+          <Table className="mt-4">
+            <thead>
+              <tr>
+                <th width="20%">Name</th>
+                <th width="10%">Price</th>
+                <th width="20%">Quantity</th>
+                {/* <th>Description</th> */}
+                <th width = "20%">AmountPayable</th>
+                <th width="10%">Actions</th>
+              </tr>
+            </thead>
+            <tbody>{cartList}</tbody>
+            <tr>
+              <th width="20%">Total Price</th>
+              <th width="20%" />
+              <th width="20%" />
+              <th width="20%">{newCart.amountPayable}</th>
+              <th>
+              <Button size="sm" color="success" type="submit" onClick={() => this.removeAll()}>
+                Empty Cart
+              </Button>
+              </th>
+            </tr>
+            
+            <tr>
+            <div className="float-left">
+              {/* {console.log("NEW CART = "+newCart)} */}
+            <Button color="success" onClick={() => this.placeOrder(cartList,newCart)}> Place order </Button>
+          </div>
+              
+              </tr>
+          </Table>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Cart;
